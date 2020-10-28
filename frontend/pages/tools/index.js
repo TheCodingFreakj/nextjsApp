23c3db2811eb83fda975cf5242ebb9054ee28d30
The CodingFreakj <pallavidapriya75@gmail.com>
2020-10-28 22:51:18 +0530

    tools pages changed

--- frontend/pages/tools/index.js
+++ frontend/pages/tools/index.js
@@ -11,6 +11,11 @@ const Tools = () => {
         <div className="row">
           <div className="col-md-12 pt-5 pb-5">
             <h2>Our Collection Of Tools </h2>
+            <h3>SEO Tools</h3>
+
+            <h3>ContentMarketing Tools</h3>
+
+            <h3>Email Marketing Tools</h3>
           </div>
         </div>
       </div>
